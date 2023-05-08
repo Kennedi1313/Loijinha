3c8ce4d33d173d09dace576106801181fd15fa05
kennedi <francisco_kennedi@hotmail.com>
2023-05-08 19:58:10 -0300

    styling floating whatsapp

--- components/contactFloating.tsx
+++ components/contactFloating.tsx
@@ -1,13 +1,13 @@
-import { BsWhatsapp } from 'react-icons/bs'
+import { BsArrowUpCircle, BsWhatsapp } from 'react-icons/bs'
 
 export default function ContactFloating() {
     return (
         <a href="https://api.whatsapp.com/send?phone=27995074930&text=Olá,%20tudo%20bem?" 
             target='blank' 
-            className='fixed right-3 bottom-20 w-12 h-12 rounded-full 
+            className='fixed right-3 bottom-14 w-14 h-14 rounded-full 
                 bg-green-500 flex items-center justify-center 
                 text-white cursor-pointer shadow-sm shadow-slate-500'>
-            <BsWhatsapp className='w-6 h-6'></BsWhatsapp>
+            <BsWhatsapp className='w-7 h-7'></BsWhatsapp>
         </a>
     )
 }
