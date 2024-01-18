f4371759d18b53acd6d1cadcf996ef4649f1cede
kennedi <francisco_kennedi@hotmail.com>
2024-01-18 16:07:52 -0300

    adding robots and maps

--- components/contactFloating.tsx
+++ components/contactFloating.tsx
@@ -1,13 +1,21 @@
-import { BsArrowUpCircle, BsWhatsapp } from 'react-icons/bs'
+import { BsArrowUpCircle, BsInstagram, BsWhatsapp } from 'react-icons/bs'
 
 export default function ContactFloating() {
     return (
-        <a href="https://api.whatsapp.com/send?phone=8498594171&text=Olá,%20tudo%20bem?%20Gostaria%20de%20algumas%20informações." 
-            target='blank' 
-            className='fixed right-3 bottom-14 w-14 h-14 rounded-full 
-                bg-rose-1000 flex items-center justify-center 
-                text-brown-1000 cursor-pointer shadow-md shadow-slate-500'>
-            <BsWhatsapp className='w-7 h-7'></BsWhatsapp>
-        </a>
+        <div>
+            <a href="https://api.whatsapp.com/send?phone=8498594171&text=Olá,%20tudo%20bem?%20Gostaria%20de%20algumas%20informações." 
+                target='blank' 
+                className='fixed right-3 bottom-14 w-14 h-14 rounded-full 
+                    bg-rose-1000 flex items-center justify-center 
+                    text-brown-1000 cursor-pointer shadow-md shadow-slate-500'>
+                <BsWhatsapp className='w-7 h-7'></BsWhatsapp>
+            </a>
+            <a className="fixed right-3 bottom-32 w-14 h-14 rounded-full 
+                    bg-rose-1000 flex items-center justify-center 
+                    text-brown-1000 cursor-pointer shadow-md shadow-slate-500" 
+                href="https://www.instagram.com/amanditapratasoficial" target='blank'>
+                <BsInstagram className='w-7 h-7'></BsInstagram>
+            </a>
+        </div>
     )
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -7,8 +7,7 @@ export default function Footer() {
     return (
         <div className='bg-rose-1000 flex flex-col items-center px-3 mt-20'>
             <div className='flex flex-col md:flex-row gap-6 justify-evenly w-fill md:w-full'>
-                <ul>
-                <div className='w-72 h-64 relative mt-2'>
+                <div className='w-72 h-52 relative mt-2'>
                     <Image 
                         src={logo}
                         alt='item'
@@ -19,7 +18,17 @@ export default function Footer() {
                         (max-width: 1200px) 50vw,
                         33vw"/>
                 </div>
-                </ul>
+            </div>
+            <div className='border-solid border-brown-1000 w-3/5 border-b-[1px] mb-10 text-center'> 
+                <span className='font-semibold'>✨ VISITE NOSSA LOJA FÍSICA ✨</span> 
+            </div>
+            <div className='w-full h-[20rem] mb-12'>
+                
+                <iframe src="https://storage.googleapis.com/maps-solutions-k9mybl2fxd/locator-plus/88wu/locator-plus.html"
+                    width="100%" height="100%"
+                    style={{border:0}}
+                    loading="lazy">
+                </iframe>
             </div>
             <div className='border-solid border-brown-1000 w-3/5 border-b-[1px] mb-10'></div>
         </div>
